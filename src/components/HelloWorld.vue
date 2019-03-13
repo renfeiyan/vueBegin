f40e61bf389581bdf65dab658884e08206ec7e3b
renfeiyan <1556792603@qq.com>
2019-03-13 14:02:01 +0800

    第一个vue页面完成

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,26 +9,11 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: '这是我的第一个页面'
+      msg: '这是我的第2个页面'
     }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+
--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,8 @@ import HelloWorld from '@/components/HelloWorld'
 import Hi from '@/components/Hi'
 import Hi1 from '@/components/Hi1'
 import Hi2 from '@/components/Hi2'
+
+
 Vue.use(Router)
 
 export default new Router({
